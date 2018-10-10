4492b880437d93ee99c764a5f73b7b763e8990e8
jburger7 <jburger77@yahoo.com>
2018-10-09 22:21:46 -0400

    TOWER DEFENSE - Initial Commit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	private Transform target;
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Seek(Transform _target){
+		target = _target;
+	}
+}
---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    private Transform target;
+
+    [Header("Attributes")]
+    public float range = 15f;
+    public float turnSpeed = 10f;
+    public float fireRate = 1f;
+    public float fireCoutdown = 0f;
+
+    [Header("Unity Setup Fields")]
+    public Transform partToRotate;
+    public string enemyTag = "Enemy";
+    public GameObject bulletPrefab;
+    public Transform firepoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        //Target Lock On and using Lerp to smooth target acquisition
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if (fireCoutdown <= 0f)
+        {
+            Shoot();
+            fireCoutdown = 1f / fireRate;
+        }
+        fireCoutdown -= Time.deltaTime;
+    }
+
+    void Shoot(){
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if(bullet != null){
+            bullet.Seek(target);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
